4d57756db538ea4636168d4d8fe5c184af4d42ae
Andrei C <andrei081993@gmail.com>
2026-08-01 10:47:23 -0700

    fix(network): add GroupFinderPackets.h to the AllPackets.h umbrella header (build failure)

--- src/server/game/Server/Packets/AllPackets.h
+++ src/server/game/Server/Packets/AllPackets.h
@@ -43,6 +43,7 @@
 #include "EquipmentSetPackets.h"
 #include "GameObjectPackets.h"
 #include "GarrisonPackets.h"
+#include "GroupFinderPackets.h"
 #include "GuildFinderPackets.h"
 #include "GuildPackets.h"
 #include "HotfixPackets.h"
